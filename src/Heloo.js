c409fb7924efd700cd2730f49d563ff97b463841
Nameer-kp <kpnameer@yahoo.com>
2020-11-04 14:54:30 +0530

    created custom component

---
+++ src/Heloo.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Hello.css';
+import 'tachyons';
+
+class Hello extends Component {
+    render(){
+        return (
+        <div className="f1 tc">
+
+            <h1>Hello world</h1>
+            
+
+        </div>
+        
+        )
+    }
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Heloo from './Heloo';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Heloo/>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
