ffb5ed89a58785fd3f57a6f188e41dd3ef30f5f0
Nameer-kp <kpnameer@yahoo.com>
2020-11-06 23:36:06 +0530

    added SearchBox component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.jsx'
+import CardList from './CardList.jsx'
+import App from './App.jsx'
 import reportWebVitals from './reportWebVitals';
-import {robots} from './robots';
+
 
 ReactDOM.render(
-  <div>
-    <Card name={robots[0].name} id ={robots[0].id} email ={robots[0].email}/>
-    <Card name={robots[1].name} id ={robots[1].id} email ={robots[1].email}/>
-    <Card name={robots[2].name} id ={robots[2].id} email ={robots[2].email}/>
-    <Card name={robots[3].name} id ={robots[3].id} email ={robots[3].email}/>
-
-
-  </div>
-  
+  <App/>
   ,document.getElementById('root')
 );
 
