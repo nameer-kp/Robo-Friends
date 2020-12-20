2a7bd87df6f3865ca0b3161b59d531aa8c92ca5d
Nameer-kp <kpnameer@yahoo.com>
2020-12-21 02:27:39 +0530

    added redux

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll'
+import Scroll from '../components/Scroll';
 import './App.css';
+import {connect} from 'react-redux';
 import ErrorBoundary from '../components/ErrorBoundary';
+import {setSearchField} from '../actions';
 
+const mapStateToProps = state =>({
+    searchField: state.searchField
+})
 
+const mapDispatchToProps = dispatch =>{
+    return {onSearch: (event)=>dispatch(setSearchField(event.target.value))}
+}
 
 class App extends Component{
     constructor(){
         super();
         this.state ={
-            robots: [],
-            searchField: ''
+            robots: []
+            
         }
     }
-
-    onSearch =(event) => {
-
-        console.log(event.target.value);
-        this.setState({searchField:event.target.value})
-       
-
-    } //we create function which should be called when we search
 
     componentDidMount () {
         console.log("check");
@@ -35,15 +35,16 @@ class App extends Component{
     }
 
     render(){
+        const {searchField,onSearch} =this.props;
         const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());    
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());    
             });
         return !this.state.robots.length ? 
         <h1>Loading</h1>:
         (
         <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox SearchChange={this.onSearch}/>
+        <SearchBox SearchChange={onSearch}/>
         <Scroll>
             <ErrorBoundary>
                 <CardList  robots={filteredRobots}/>
@@ -57,4 +58,4 @@ class App extends Component{
         
     }
 }
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps) (App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import App from './containers/App.jsx'
 import reportWebVitals from './reportWebVitals';
+import {searchRobot} from './reducers'
 
-
+const store = createStore(searchRobot)
 ReactDOM.render(
-  <App/>
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  
   ,document.getElementById('root')
 );
 
